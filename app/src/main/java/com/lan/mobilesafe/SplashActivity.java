0a9bc0dcafc487102f2bb26061aacbd0ec3e8c79
lan <519202180@qq.com>
2016-05-22 23:12:56 +0800

    Splash界面初步

--- app/src/main/java/com/lan/mobilesafe/SplashActivity.java
+++ app/src/main/java/com/lan/mobilesafe/SplashActivity.java
@@ -1,9 +1,10 @@
 package com.lan.mobilesafe;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
